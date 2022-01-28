b2fbdbf2eedaeea044a9bac81436a963449fb698
Tuhin Mridha <tuhinmridha11@gmail.com>
2022-01-28 12:06:42 +0600

    Snow Desert City Map Completed

--- Assets/Script/ChangeCar.cs
+++ Assets/Script/ChangeCar.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ChangeCar : MonoBehaviour
 {
+    public GameObject[] cars;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cars[Random.Range(0, cars.Length)].SetActive(true);
     }
 
     // Update is called once per frame
